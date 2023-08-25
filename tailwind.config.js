252144a5d19d29559d557411185e27239d583515
Tori-dev <victory.codess@gmail.com>
2023-08-25 13:21:32 +0100

    complete dashboard ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
     screens: {
-sm: '480px',
-md: '768px',
-lg: '1045px',
-xl: '1300px'
+      sm: "480px",
+      md: "768px",
+      lg: "1045px",
+      xl: "1300px",
     },
     extend: {
       colors: {
-        primary: '#FBCC34',
-        white: '#FFFFFF',
-        black: '#000000',
-        grey: '#758173',
+        primary: "#FBCC34",
+        white: "#FFFFFF",
+        black: "#000000",
+        grey: "#758173",
       },
-backgroundImage: {
-        'breadcrum': "url('/assets/img/breadcrum.png')",
-        
-      }
+      backgroundImage: {
+        breadcrum: "url('/assets/img/breadcrum.png')",
+        electbg: "url('/assets/img/electricalbg.png')",
+        homeautobg: "url('/assets/img/homeautobg.png')",
+        solarbg: "url('/assets/img/solarinstbg.png')",
+        cctvbg: "url('/assets/img/cctvinstbg.png')",
+      },
     },
   },
   plugins: [],
-}
-
+};
