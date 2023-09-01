fd014edf72647bd1e7cc5a90f7615e3e4cf971ee
Tori-dev <victory.codess@gmail.com>
2023-09-01 16:49:10 +0100

    compressed images

--- assets/scripts/testimonial.js
+++ assets/scripts/testimonial.js
@@ -3,26 +3,26 @@ const testimonials = [
   {
     name: "Eva Sawyer",
     job: "CEO, Fashworks",
-    image: "/assets/img/testim.png",
+    image: "/assets/img/testim.webp",
     testimonial:
       "Neque volutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur",
   },
   {
     name: "Katey Topaz",
     job: "Developer, TechCrew",
-    image: "/assets/img/testim.png",
+    image: "/assets/img/testim.webp",
     testimonial:
       "Elementum tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse in est ante in nibh mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit amet nulla",
   },
   {
     name: "Tori Dev",
     job: "CEO, TechCrew",
-    image: "/assets/img/testim.png",
+    image: "/assets/img/testim.webp",
     testimonial:
       "Elementum tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse in est ante in nibh mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit amet nulla",
   },
 ];
- // Current Slide
+// Current Slide
 let i = 0;
 // Total Slides
 let j = testimonials.length;
