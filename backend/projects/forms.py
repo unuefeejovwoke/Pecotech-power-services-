60e1e5fd661fdc88be760b867f929cf6fd9eeb85
fredtruix <fredricktrust@gmail.com>
2023-10-18 07:18:31 +0100

    fix: errorrs

--- backend/projects/forms.py
+++ backend/projects/forms.py
@@ -16,10 +16,9 @@ class ServiceRequestForm(forms.ModelForm):
 
     class Meta:
         model = ServiceRequest
-        fields = ['full_name', 'email', 'phone_number', 'quotation_info', 'service_required', 'uploaded_files']
+        fields = ['full_name', 'email', 'phone_number',  'service_required', 'uploaded_files']
         widgets = {
             'full_name': forms.TextInput(attrs={'class': 'w-full border mt-1 mb-3 border-black rounded-lg outline-none p-2', 'placeholder': 'Full Name'}),
             'email': forms.EmailInput(attrs={'class': 'w-full border mt-1 mb-3 border-black rounded-lg outline-none p-2', 'placeholder': 'Email Address'}),
             'phone_number': forms.TextInput(attrs={'class': 'w-full border mt-1 mb-3 border-black rounded-lg outline-none p-2', 'placeholder': 'Phone Number'}),
-            'quotation_info': forms.Textarea(attrs={'class': 'w-full border mt-1 mb-3 border-black rounded-lg outline-none p-2', 'placeholder': 'Quotation Info'}),
         }
